34def1f0081344ceeedfa3045a7697edc6293c90
sskurochkin <semenkurochkin@gmail.com>
2022-02-04 09:57:38 +0300

    add method form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,78 +1,82 @@
 class Parallel {
-    constructor(p) {
-        this.parallelJobs = p.parallelJobs;
-    }
+	constructor(p) {
+		this.parallelJobs = p.parallelJobs;
+		this.results = [];
+		this.jobs = [];
+		this.activeJobs = 0;
+		this.onDone;
+		this.index = 0;
+	}
 
-    result = [];
-    jobs = [];
-    jobsNow = 0;
-    doneJob = undefined;
-    jobCount = [];
+	go() {
+		for (
+			let i = 0;
+			i < Math.min(this.parallelJobs, this.jobs.length);
+			i++
+		) {
+			this.next();
+		}
+	}
 
-    job(step) {
-        let t = this;
-        // if (!this.jobCount.includes(step)) {
-        //     this.jobCount.push(step);
-        // }
-        // if (this.jobsNow < this.parallelJobs) {
-        //     step(t);
-        //     this.jobsNow++;
-        //     return this;
-        // }
+	next() {
+		const index = this.index;
+		this.activeJobs++;
 
-        // this.jobs.push(step);
-        async function run() {
-            const go = console.log(1)
-            await step(go)
-            t.jobs.push(step);
-        }
-        run()
+		this.jobs[this.index]((result) => this.onResult(result, index));
 
+		this.index++;
+	}
 
+	onResult(result, index) {
+		this.results[index] = result;
+		this.activeJobs--;
 
-        console.log(this.jobs);
+		if (this.jobs[this.index]) {
+			this.next();
+		} else if (this.activeJobs === 0) {
+			this.onDone(this.results);
+		}
+	}
 
-        return this;
-    }
+	job(step) {
+		this.jobs.push(step);
+		return this;
+	}
 
-    done(param) {
-        // this.jobs.forEach((elem) => {
-        //     // console.log(this)
-        //     this.jobCount.push(elem());
-        //     console.log(this.jobCount);
-        // });
-        param(this.jobs);
-    }
+	done(onDone) {
+		this.onDone = onDone;
+		this.go();
+	}
 }
 
 const runner = new Parallel({
-    parallelJobs: 2,
+	parallelJobs: 4,
 });
 runner.job(step1).job(step2).job(step3).job(step4).done(onDone);
 // runner.job(step1).job(step2).job(step3).job(step4);
 
 function step1(done) {
-    setTimeout(done, 100, "step1");
+	setTimeout(done, 100, "step1");
 }
 
 function step2(done) {
-    setTimeout(done, 10, "step2");
+	setTimeout(done, 10, "step2");
 }
 
 function step3(done) {
-    setTimeout(done, 150, "step3");
+	setTimeout(done, 150, "step3");
 }
 
 function step4(done) {
-    setTimeout(done, 50, "step4");
+	setTimeout(done, 50, "step4");
 }
 
 function onDone(results) {
-    console.log(results);
-    console.assert(Array.isArray(results), "result must be an array");
-    console.assert(results.length == 4, "Wrong count of answers");
-    console.assert(results[0] === "step1", "Wrong answer 1");
-    console.assert(results[1] === "step2", "Wrong answer 2");
-    console.assert(results[2] === "step3", "Wrong answer 3");
-    console.assert(results[3] === "step4", "Wrong answer 4");
+	console.log(results);
+	console.assert(Array.isArray(results), "result must be an array");
+	console.assert(results.length == 4, "Wrong count of answers");
+	console.assert(results[0] === "step1", "Wrong answer 1");
+	console.assert(results[1] === "step2", "Wrong answer 2");
+	console.assert(results[2] === "step3", "Wrong answer 3");
+	console.assert(results[3] === "step4", "Wrong answer 4");
 }
